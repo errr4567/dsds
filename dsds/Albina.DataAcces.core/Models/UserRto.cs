734dbddf6b52bf04fd91a15b23041b3da1c1ad43
Rsklgi <82046260+errr4567@users.noreply.github.com>
2021-10-28 19:24:17 +0300

    proekt
    d

---
+++ dsds/Albina.DataAcces.core/Models/UserRto.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Albina.DataAcces.core.Models
+{
+    [Table("User")]
+    class UserRto
+    {
+        
+        [Key]
+        public int id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Surname { get; set; }
+
+        public string PhoneNumberPrefex { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+    }
+}
